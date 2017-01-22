f3fb118488ac49abfad263feb36defc0b8a2d4d7
Emre <Emre@Emre-PC>
2017-01-22 20:13:10 +0200

    Ayrıştırılacak metnin program içinde bir değişkene eklendiği durumda çalışan hali.

--- StringIslemleri/src/com/vektorel/java/string/StringIslemleri.java
+++ StringIslemleri/src/com/vektorel/java/string/StringIslemleri.java
@@ -4,26 +4,31 @@ public class StringIslemleri {
 
 	public static void main(String[] args) {
 		final String metin = 
-				"#PatNo:1234@Pat: Taner TEMEL@Age:45@HBG:65@AG:45@#"
-				+ "#PatNo:1235@Pat: Kemal TEMEL@Age:34@HBG:65@AG:45@#"
-				+ "#PatNo:1236@Pat: Cemal TEMEL@Age:23@HBG:65@AG:45@#"
-				+ "#PatNo:1237@Pat: Ayþe TEMEL@Age:5@HBG:65@AG:45@#"
-				+ "#PatNo:1238@Pat: Fatma TEMEL@Age:75@HBG:65@AG:45@#";
+			"@PatNo:234562@Pat:Taner TEMEL@Age:45@HBG:23@AG:45@#" +
+			"@PatNo:234563@Pat:Kemal TEMEL@Age:34@HBG:23@AG:45@#" +
+			"@PatNo:234564@Pat:Cemal TEMEL@Age:23@HBG:23@AG:45@#" + 
+			"@PatNo:234565@Pat:Ayþe TEMEL@Age:5@HBG:23@AG:45@#  " +
+			"@PatNo:234566@Pat:Fatma TEMEL@Age:75@HBG:23@AG:45@#" +
+			"@PatNo:234562@Pat:Taner TEMEL@Age:45@HBG:23@AG:45@#" +
+			"@PatNo:234563@Pat:Kemal TEMEL@Age:34@HBG:23@AG:45@#" +
+			"@PatNo:234565@Pat:Ayþe TEMEL@Age:5@HBG:23@AG:45@#  " +
+			"@PatNo:234566@Pat:Fatma TEMEL@Age:75@HBG:23@AG:45@#";
+		
 	
 		String[] satirlar = metin.split("#");
 		
 		for (int i = 0; i < satirlar.length; i++) {
 			String[] kelimeler = satirlar[i].split("@");
 
-			for (int j = 0; j < kelimeler.length; j++) {
-				System.out.println("Hasta No: " + kelimeler[j].split(":")[1]);
-				System.out.println("Adý Soyadý: ");
-				System.out.println("Yaþý: ");
-				System.out.println("HBG: ");
-				System.out.println("AG: ");			
-			}
+			System.out.println("\n----------------------------------------");
 			
+			System.out.println("Hasta No: " + kelimeler[1].split(":")[1]);
+			System.out.println("Adý Soyadý: " + kelimeler[2].split(":")[1]);
+			System.out.println("Yaþý: " + kelimeler[3].split(":")[1]);
+			System.out.println("HBG: " + kelimeler[4].split(":")[1]);
+			System.out.println("AG: " + kelimeler[5].split(":")[1]);
 			
+			System.out.println("----------------------------------------");
 		}
 		
 		
